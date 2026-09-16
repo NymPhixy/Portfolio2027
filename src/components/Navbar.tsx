c6ae05f7d6ec50db417d89ce8d4799fb50bf2a24
Ruben Janssen <jouw-email@example.com>
2026-09-16 12:47:11 +0200

    RGB Visuals navigation.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <a href="#home">RGB Visuals</a>
+
+      <div>
+        <a href="#home">Home</a>
+        <a href="#projects">Projecten</a>
+        <a href="#about">Over mij</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
